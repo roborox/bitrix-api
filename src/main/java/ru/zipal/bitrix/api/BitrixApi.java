7ed8a1955fff76eef617ff9026944489d8fe43f4
Roborox <dev@roborox.org>
2016-06-01 17:36:52 +0300

    support for custom bitrix contacts

--- src/main/java/ru/zipal/bitrix/api/BitrixApi.java
+++ src/main/java/ru/zipal/bitrix/api/BitrixApi.java
@@ -25,7 +25,7 @@ public class BitrixApi {
         this.serializer = serializer;
     }
 
-    public BitrixPage<BitrixContact> listContacts(String domain, Tokens tokens, Integer start, String phone) throws BitrixApiException {
+    public <T extends BitrixContact> BitrixPage<T> listContacts(Class<T> contactClass, String domain, Tokens tokens, Integer start, String phone) throws BitrixApiException {
         final List<NameValuePair> params = new ArrayList<>();
         if (start != null) {
             params.add(new BasicNameValuePair("start", start.toString()));
@@ -33,7 +33,11 @@ public class BitrixApi {
         if (phone != null) {
             params.add(new BasicNameValuePair("filter[PHONE]", phone));
         }
-        return getPage(client.execute(domain, "crm.contact.list", params, tokens), BitrixContact.class);
+        return getPage(client.execute(domain, "crm.contact.list", params, tokens), contactClass);
+    }
+
+    public BitrixPage<BitrixContact> listContacts(String domain, Tokens tokens, Integer start, String phone) throws BitrixApiException {
+        return listContacts(BitrixContact.class, domain, tokens, start, phone);
     }
 
     public BitrixPage<BitrixLead> listLeads(String domain, Tokens tokens, Integer start, String phone) throws BitrixApiException {
@@ -55,8 +59,12 @@ public class BitrixApi {
         return getPage(client.execute(domain, "user.get", params, tokens), BitrixUser.class);
     }
 
+    public <T extends BitrixContact> T getContact(Class<T> contactClass, String domain, Tokens tokens, long id) throws BitrixApiException {
+        return serializer.deserialize(contactClass, client.execute(domain, "crm.contact.get", Collections.singletonList(new BasicNameValuePair("id", Long.toString(id))), tokens).getJSONObject("result"));
+    }
+
     public BitrixContact getContact(String domain, Tokens tokens, long id) throws BitrixApiException {
-        return serializer.deserialize(BitrixContact.class, client.execute(domain, "crm.contact.get", Collections.singletonList(new BasicNameValuePair("id", Long.toString(id))), tokens).getJSONObject("result"));
+        return getContact(BitrixContact.class, domain, tokens, id);
     }
 
     public void removeContact(String domain, Tokens tokens, long id) throws BitrixApiException {
