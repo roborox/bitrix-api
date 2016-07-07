8f34a885cf55843e36384282247cf6d8762075a7
Roborox <dev@roborox.org>
2016-07-07 13:19:01 +0300

    deploy

--- src/test/java/ru/zipal/bitrix/serializer/ChildActivity.java
+++ src/test/java/ru/zipal/bitrix/serializer/ChildActivity.java
@@ -1,6 +1,17 @@
 package ru.zipal.bitrix.serializer;
 
+import ru.zipal.bitrix.api.common.FieldName;
 import ru.zipal.bitrix.api.model.BitrixActivity;
 
 public class ChildActivity extends BitrixActivity {
+    @FieldName("UF_CRM_1439291710")
+    private Integer number;
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
 }
